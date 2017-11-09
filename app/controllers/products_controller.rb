465c00c0458fb1f682d6b26b8e19938654ac5f27
Bhanu <bhanusruti512@gmail.com>
2017-11-08 23:21:56 -0600

    add show action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    id = params[:id]
+    @product = Product.find_by(id: id)
+  end
 end
